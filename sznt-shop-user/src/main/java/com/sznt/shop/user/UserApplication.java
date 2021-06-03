fb8d851b13de2e4e8bbfd23a895a1ef613a258ae
cbqr <liusheng_work@163.com>
2021-06-04 00:11:44 +0800

    user模块启动类加上debug bean日志打印配置

--- sznt-shop-user/src/main/java/com/sznt/shop/user/UserApplication.java
+++ sznt-shop-user/src/main/java/com/sznt/shop/user/UserApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.Banner;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
- * 用户服务
+ * 用户中心
  *
  * @author S Mr.L 
  * @since 2021-05-31
@@ -16,11 +17,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 @SpringBootApplication
 public class UserApplication {
     public static void main(String[] args) {
-        log.info("===sznt-shop 用户服务开始启动===");
-        new SpringApplicationBuilder(UserApplication.class)
+        log.info("=== [sznt-shop] 用户中心开始启动 ===");
+        boolean logFlag = log.isDebugEnabled();
+        ConfigurableApplicationContext run = new SpringApplicationBuilder(UserApplication.class)
                 .web(WebApplicationType.SERVLET)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
-        log.info("===sznt-shop 用户服务启动成功===");
+        if (logFlag) {
+            int counts = run.getBeanDefinitionCount();
+            String[] names = run.getBeanDefinitionNames();
+            log.debug("[sznt-shop] 用户中心IOC容器一共加载了 [{}] 个组件, 组件的名字分别为: [{}]", counts, names);
+        }
+        log.info("=== [sznt-shop] 用户中心启动完成 ===");
     }
 }
